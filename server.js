359b27f5d87e64f4093d2b3eb9eeadc3030a74e1
TamarFrenkel <tftf9790tftf@gmail.com>
2025-09-15 20:11:42 +0300

    Update server code

--- server.js
+++ server.js
@@ -1,19 +1,19 @@
-require('dotenv').config();
-
 const express = require("express");
 const app = express();
 const mongoose = require('mongoose');
 const cors = require('cors');
+require('dotenv').config();
+
 //אם המסד נתונים הזה קיים הוא יתחבר אליו,
 //אם לא קיים הוא ייצר לי אותו
-mongoose.connect(process.env.MONGO_URI)
-    .then(()=>{
-        console.log('connect to DB');
-    app.listen(5000, () =>{
-    console.log('server is running in port: ${process.env.PORT}');
-    })
-    })  
-    .catch((err) => console.error('❌ MongoDB connection error:', err.message));
+mongoose.connect(process.env.MONGO_URI, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true
+})
+.then(() => console.log('MongoDB connected'))
+.catch(err => console.error('MongoDB connection error:', err));
+
+require('dotenv').config();
 
 const workerRouter = require('./routes/workerRouter');
 const dishRouter = require('./routes/dishRouter');
@@ -26,4 +26,3 @@ app.use('/api/workers', workerRouter);
 app.use('/api/dishes', dishRouter);
 app.use('/api/tasks', taskRouter);
 app.use('/images', express.static('public/images'));
-
